3e09e4f447c62309b8c18c454d8c91a9b5fa0930
Stuart Ingram <ingrams2@upmc.com>
2014-01-19 10:22:48 -0500

    Linked up sentiment analysis to twitter search with visualization of sentiment.

--- routes/main.rb
+++ routes/main.rb
@@ -9,7 +9,7 @@ class Sentiment < Sinatra::Application
     content_type :json
     puts "Searching tweets for '#{params['search_term']}'"
     tweets = TwitterSearch.search(params['search_term'])
-    tweets.map! { |t| { text: t } }
+    tweets.map! { |t| { text: t, sentiment_css: sentiment_to_css(Sentiment.extract(t).to_s) } }
     puts "Found #{tweets}"
     { search_term: params['search_term'], tweets: tweets }.to_json
   end
@@ -18,6 +18,17 @@ class Sentiment < Sinatra::Application
     content_type :json
     sentiment = Sentiment.extract(params['search_term'])
     { sentiment: sentiment.to_s }.to_json
+  end
+
+  def sentiment_to_css(sentiment)
+    case sentiment
+    when 'very negative' then 'alert-danger'
+    when 'negative'      then 'alert-danger'
+    when 'neutral'       then 'alert-warning'
+    when 'positive'      then 'alert-success'
+    when 'very positive' then 'alert-success'
+    else ''
+    end
   end
 
 end
